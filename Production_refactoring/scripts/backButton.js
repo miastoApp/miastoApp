a02ad35e4e027910d9f658aa78f0dc0cdd6eb9f6
Arkadiusz Węgrzyn <wegrzyn.ark@gmail.com>
2017-05-08 10:51:57 +0200

    Dodana wersja po refactoringu wyglądu (scss do zminifikowanego css, prefixy dla css, geolokalizacja etc. bla bla bla)

---
+++ Production_refactoring/scripts/backButton.js
@@ -0,0 +1,18 @@
+/**
+ * Created by ricevind on 04.03.17.
+ */
+
+(function() {
+    "use strict";
+    const backHref = document.querySelector('#backButton');
+
+    if (window.history) {
+        backHref.addEventListener('click', function(e){
+
+            e.preventDefault();
+            window.history.back();
+        }, false);
+    } else {
+        backHref.setAttribute('href', 'main.html');
+    }
+})();
---
+++ Production_refactoring/scripts/hamburger.js
@@ -0,0 +1,21 @@
+(function () {
+  "use strict";
+
+  var hamburger = document.querySelector('#hamburger');
+  var menu = document.querySelector('#menu');
+  var menuWrapper = document.querySelector('.menu-wrapper');
+
+  function toggleClass(elems, className) {
+    elems.map(function (elem) {
+      elem.classList.toggle(className)
+
+    });
+  }
+
+  hamburger.addEventListener('click', function(e){
+    "use strict";
+    e.preventDefault();
+    toggleClass([hamburger, menu, menuWrapper], 'open');
+  });
+
+})();
